c96adbdd68ea9f35712b74cb49c7f307dd0dde88
kamiliano1 <kamiliano1991@gmail.com>
2023-02-16 22:05:30 +0100

    Home
    Created Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,37 @@
-import { useContext } from "react"
-import { CountryContext } from "../Context"
-import Navbar from "../components/Navbar"
+import { useContext } from 'react'
+import { CountryContext } from '../Context'
+import { Link } from "react-router-dom"
 import SearchBar from '../components/SearchBar'
 import List from '../components/List'
+import Country from '../components/Country'
+
 export default function Home() {
+    const { allCountryData, countryDetails } = useContext(CountryContext)
+    const allcountries = allCountryData.map(country=>{
+        return (
+          <Link to={`/${country.name}`} key={country.id}>
+            <Country
+              id={country.id}
+              flag={country.flag}
+              name={country.name}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              onClick={()=>countryDetails(country.id)}
+            />
+          </Link>
+        )
+    }
+    )
     return (
-    <div>
-    <Navbar />
-      <div className='w-[90%] mx-auto'>
-        <SearchBar />
-        <List />
-      </div></div>
+    <div className='w-[90%] mx-auto'>
+        <section className="md:flex items-center justify-between ">
+            <SearchBar/>
+            <List />
+        </section>
+        <main className='grid gap-8 md:gap-16 md:grid-cols-auto-fill md:pb-10 max-[570px]:w-[90%] max-[570px]:mx-auto'>
+            {allcountries}
+        </main>
+    </div>
     )
 }
