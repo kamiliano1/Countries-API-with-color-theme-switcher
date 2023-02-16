8fc7d677d2b254836f24f6e3a8fe1800ff375c91
kamiliano1 <kamiliano1991@gmail.com>
2023-02-16 22:06:19 +0100

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,19 @@
 import { useContext } from "react"
 import { CountryContext } from "../Context"
+import LoopIcon from "../Icons/LoopIcon"
 export default function SearchBar() {
-    function MdiMagnify(props) {
-        return (
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z"></path></svg>
-        )
-      }
-    const { darkMode, toggleDarkMode, handleChange, searchBarValue, handleSubmit } = useContext(CountryContext)
+    const { handleChange, searchBarValue, handleSubmit } = useContext(CountryContext)
     return (
-        <form action="" onSubmit={handleSubmit} className="text-black dark:text-white dark:bg-darkBlue bg-white
-        flex px-8 h-[50px] w-[100%] md:w-[20%] rounded-lg my-5">
-            <button type="submit" className="text-2xl"><MdiMagnify/></button>
+        <form action="" onSubmit={handleSubmit} className="text-black bg-white dark:text-white dark:bg-darkBlue 
+        flex px-8 h-[50px] w-[100%] md:w-[35%] rounded-lg my-5">
+            <button type="submit" className="text-2xl"><LoopIcon/></button>
             <input 
             type="text" 
             name="countryName"
             onChange={handleChange}
             value={searchBarValue} 
             placeholder="Search for a country..." 
-            className="ml-4 py-1 dark:bg-darkBlue bg-white placeholder-black  dark:placeholder-white text-sm rounded-lg" />
+            className="ml-4 py-1 dark:bg-darkBlue bg-white placeholder-black dark:placeholder-white text-sm rounded-lg" />
         </form>
     )
 }
