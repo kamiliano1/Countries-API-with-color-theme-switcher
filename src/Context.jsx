023e0d52dde9d826e868eac34dd4b72335584194
kamiliano1 <kamiliano1991@gmail.com>
2023-02-16 22:06:29 +0100

    Update Context.jsx

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,14 +1,11 @@
-import { list } from "postcss"
 import { createContext, useState, useEffect } from "react"
 
 const CountryContext = createContext()
 
-
 function CountryContextProvider(props) {
 
-    // const [countryData, setCountryData] = useState([])
     const [allCountryData, allSetCountryData] = useState([])
-    const [darkMode, setDarkMode] = useState(false)
+    const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("isDarkActive")) || false)
     const [searchBarValue, setSearchBarValue] = useState("")
     const [fetchLink, setFetchLink] = useState("https://restcountries.com/v2/all")
     const [allCountry, setAllCountry] = useState([])
@@ -23,83 +20,57 @@ function CountryContextProvider(props) {
     setSearchBarValue("")
 }
 
+
     function toggleDarkMode() {
       setDarkMode(prevMode=>!prevMode)
+      
     }
 
     function updateListValue(value) {
       allSetCountryData(value==="all" ? allCountry : allCountry.filter(country=>country.region===value))
+      setSearchBarValue("")
     }
-
-    useEffect(()=>{
+    function fetchData(fetchLink, fetchData) {
       fetch(fetchLink)
-        .then(res => res.json())
-        .then(data=> allSetCountryData(
-                data.map((country, id)=>{
-                const borderCountry = country.borders ? country.borders.map(border=>border).join(",") : "None"
-                const currencies = country.currencies ? country.currencies.map(currency=>currency.name) : "None"
-                const languages = country.languages.map(language=>language.name).join(", ")
-            return {
-                    id:id,
-                    name:country.name,
-                    nativeName: country.nativeName,
-                    population: country.population.toLocaleString('en-US'),
-                    region: country.region,
-                    subregion: country.subregion,
-                    capital: country.capital,
-                    topLevelDomain: country.topLevelDomain,
-                    currencies: currencies,
-                    language: languages,
-                    borders: borderCountry,
-                    flag: country.flag,
-                }})
-            
-
+      .then(res => res.json())
+      .then(data=> fetchData(data.map((country, id)=>{
+              const borderCountry = country.borders ? country.borders.map(border=>border).join(",") : "None"
+              const currencies = country.currencies ? country.currencies.map(currency=>currency.name) : "None"
+              const languages = country.languages.map(language=>language.name).join(", ")
+          return {
+                  id:id,
+                  name:country.name,
+                  nativeName: country.nativeName,
+                  population: country.population.toLocaleString('en-US'),
+                  region: country.region,
+                  subregion: country.subregion,
+                  capital: country.capital,
+                  topLevelDomain: country.topLevelDomain,
+                  currencies: currencies,
+                  language: languages,
+                  borders: borderCountry,
+                  flag: country.flag,
+                }   
+              })
             )
-                
-    )
-    // setCurrentSearch(allCountryData)
-    // setCurrentSearch(allCountry)
-    },[fetchLink])
+          )     
+    }
+    useEffect(()=>{
+      localStorage.setItem("isDarkActive", JSON.stringify(darkMode))
+    }, [darkMode])
 
     useEffect(()=>{
-      fetch("https://restcountries.com/v2/all")
-      .then(res =>res.json())
-      .then(data=> setAllCountry(data.map((country, id)=>{
-            const borderCountry = country.borders ? country.borders.map(border=>border) : "None"
-            const currencies = country.currencies? country.currencies.map(currency=>currency.name) : "None"
-            const languages = country.languages.map(language=>language.name).join(", ")
-        return {
-                id:id,
-                name:country.name,
-                nativeName: country.nativeName,
-                population: country.population.toLocaleString('en-US'),
-                region: country.region,
-                subregion: country.subregion,
-                capital: country.capital,
-                topLevelDomain: country.topLevelDomain,
-                currencies: currencies,
-                language: languages,
-                borders: borderCountry,
-                flag: country.flag
-            }})
-
-        )
-        )
-        
-        // console.log(allCountry)
+      fetchData("https://restcountries.com/v2/all", setAllCountry)
     },[])
 
-
-
-
-
+    useEffect(()=>{
+      fetchData(fetchLink, allSetCountryData)
+    },[fetchLink])
 
     return (
         <CountryContext.Provider value={{
-        allCountryData, allCountry, darkMode, toggleDarkMode, 
-        updateListValue,searchBarValue, handleChange, 
-        handleSubmit,
+          allCountryData, darkMode, allCountry, handleChange, searchBarValue, handleSubmit, toggleDarkMode, updateListValue, 
+        
         }}>
             {props.children}
         </CountryContext.Provider>
